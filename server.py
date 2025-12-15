93f79d57d985773514afdee90b41d808ede38552
anna-rodriguez <anna.rodriguez@ibm.com>
2025-12-14 21:41:53 -0500

    server info

---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+app = Flask("Emotion Detection")
+@app.route("\emotionDetector")
+def em_detect():
+    text_to_analyze = request.args.get('textToAnalyze')
+ 
+     # Pass the text to the emotion_detector
+    response = emotion_detector(text_to_analyze)
+
+    # Extract scores and dominant emotion
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant = response['dominant_emotion']
+
+    # Return the formatted string
+    return (
+        f"For the given statement, the system response is "
+        f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, "
+        f"'joy': {joy} and 'sadness': {sadness}. "
+        f"The dominant emotion is {dominant}."
+    )
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
